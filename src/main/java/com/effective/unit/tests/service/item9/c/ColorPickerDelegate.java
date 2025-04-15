a6070330b2a138650d08b4e5d4e48e018ae91da0
Ashley-Waldron <ashley.waldron@gmail.com>
2025-04-15 18:43:49 +0100

    Create ColorPickerDelegate.java

---
+++ src/main/java/com/effective/unit/tests/service/item9/c/ColorPickerDelegate.java
@@ -0,0 +1,8 @@
+package com.effective.unit.tests.service.item9.c;
+
+public class ColorPickerDelegate {
+
+    public String pick(String colorName) {
+        return ColorPickerUtils.pick(colorName);
+    }
+}
