245abc1fcc8af3da7504b19523419ea6185683d7
Ashley-Waldron <ashley.waldron@gmail.com>
2025-04-15 18:21:17 +0100

    Update EmployeeNumberGenerator.java

--- src/main/java/com/effective/unit/tests/service/EmployeeNumberGenerator.java
+++ src/main/java/com/effective/unit/tests/service/EmployeeNumberGenerator.java
@@ -1,1 +1,11 @@
+package com.effective.unit.tests.service;
 
+public class EmployeeNumberGenerator {
+    public String generate(String region) {
+        return null;
+    }
+
+    public String someOtherMethod() {
+        return null;
+    }
+}
