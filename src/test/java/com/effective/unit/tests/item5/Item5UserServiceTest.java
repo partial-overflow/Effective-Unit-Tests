7ceae94fac39d87c7c0c7780cb3e3d7c78cc7bb0
Ashley-Waldron <ashley.waldron@gmail.com>
2025-04-16 17:30:57 +0100

    Create Item5UserServiceTest.java

---
+++ src/test/java/com/effective/unit/tests/item5/Item5UserServiceTest.java
@@ -0,0 +1,82 @@
+package com.effective.unit.tests.service.item5;
+
+import com.effective.unit.tests.domain.CreateUserProfileRequest;
+import com.effective.unit.tests.domain.CreateUserProfileResponse;
+import com.effective.unit.tests.domain.UserEntity;
+import com.effective.unit.tests.domain.enums.EventType;
+import com.effective.unit.tests.repositories.UserRepository;
+import com.effective.unit.tests.service.EmployeeNumberGenerator;
+import com.effective.unit.tests.service.EventBroadcaster;
+import com.effective.unit.tests.service.MarketingEmailService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.effective.unit.tests.service.item5.TestDataCreator.TEST_EMAIL_ADDRESS;
+import static com.effective.unit.tests.service.item5.TestDataCreator.TEST_EMPLOYEE_NUMBER;
+import static com.effective.unit.tests.service.item5.TestDataCreator.TEST_REGION;
+import static com.effective.unit.tests.service.item5.TestDataCreator.TEST_UUID;
+import static com.effective.unit.tests.service.item5.TestDataCreator.UUID_PATTERN;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+public class Item5UserServiceTest {
+
+    @InjectMocks
+    private Item5UserService item5UserService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private EventBroadcaster eventBroadcaster;
+
+    @Mock
+    private MarketingEmailService emailService;
+
+    @Mock
+    private EmployeeNumberGenerator employeeNumberGenerator;
+
+    @Captor
+    private ArgumentCaptor<UserEntity> userEntityArgumentCaptor;
+
+    private UserEntity mockSavedUserEntity;
+    private CreateUserProfileRequest createUserProfileRequest;
+
+    @BeforeEach
+    void setup() {
+        mockSavedUserEntity = TestDataCreator.createTestUserEntity();
+        createUserProfileRequest = TestDataCreator.createTestCreateUserProfileRequest();
+    }
+
+    @Test
+    void createSuccessItem5a() {
+        given(userRepository.save(userEntityArgumentCaptor.capture())).willReturn(mockSavedUserEntity);
+        given(employeeNumberGenerator.generate(TEST_REGION)).willReturn(TEST_EMPLOYEE_NUMBER);
+
+        CreateUserProfileResponse createUserProfileResponse = item5UserService.create(createUserProfileRequest);
+
+        assertThat(createUserProfileResponse.getEmployeeNumber(), is(equalTo(TEST_EMPLOYEE_NUMBER)));
+        assertThat(createUserProfileResponse.getEmailAddress(), is(equalTo(TEST_EMAIL_ADDRESS)));
+        assertThat(createUserProfileResponse.getRegion(), is(equalTo(TEST_REGION)));
+        assertThat(createUserProfileResponse.getId(), is(equalTo(TEST_UUID)));
+
+        assertThat(userEntityArgumentCaptor.getValue().getEmployeeNumber(), is(equalTo(TEST_EMPLOYEE_NUMBER)));
+        assertThat(userEntityArgumentCaptor.getValue().getEmailAddress(), is(equalTo(TEST_EMAIL_ADDRESS)));
+        assertThat(userEntityArgumentCaptor.getValue().getRegion(), is(equalTo(TEST_REGION)));
+        assertThat(userEntityArgumentCaptor.getValue().getId(), matchesPattern(UUID_PATTERN));
+
+        then(eventBroadcaster).should().broadcast(EventType.USER_REGISTRATION, mockSavedUserEntity);
+        then(emailService).should().registerSubscriber(mockSavedUserEntity);
+    }
+}
