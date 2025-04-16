8f3ff210cf3681e0159450d01c80cadac6e93d4a
Ashley-Waldron <ashley.waldron@gmail.com>
2025-04-16 17:31:21 +0100

    Create TestDataCreator.java

---
+++ src/test/java/com/effective/unit/tests/item5/TestDataCreator.java
@@ -0,0 +1,32 @@
+package com.effective.unit.tests.service.item5;
+
+import com.effective.unit.tests.domain.CreateUserProfileRequest;
+import com.effective.unit.tests.domain.UserEntity;
+
+import java.util.UUID;
+
+public class TestDataCreator {
+
+    public static final String UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+    public static final String TEST_EMPLOYEE_NUMBER = "EUS1002";
+    public static final String TEST_REGION = "US-EAST";
+    public static final String TEST_EMAIL_ADDRESS = "TestEmailAddress@test.com";
+    public static final String TEST_UUID = UUID.randomUUID().toString();
+
+    public static CreateUserProfileRequest createTestCreateUserProfileRequest() {
+        CreateUserProfileRequest createUserProfileRequest = new CreateUserProfileRequest();
+        createUserProfileRequest.setEmailAddress(TEST_EMAIL_ADDRESS);
+        createUserProfileRequest.setRegion(TEST_REGION);
+        createUserProfileRequest.shouldReceiveMarketingEmails(true);
+        return createUserProfileRequest;
+    }
+
+    public static UserEntity createTestUserEntity() {
+        UserEntity mockSavedUserEntity = new UserEntity();
+        mockSavedUserEntity.setEmployeeNumber(TEST_EMPLOYEE_NUMBER);
+        mockSavedUserEntity.setRegion(TEST_REGION);
+        mockSavedUserEntity.setEmailAddress(TEST_EMAIL_ADDRESS);
+        mockSavedUserEntity.setId(TEST_UUID);
+        return mockSavedUserEntity;
+    }
+}
